fab7aa27c1f8056332b499d76b250314615437e3
tacheraSasi <tacherasasi@gmail.com>
2024-12-16 00:47:52 +0300

    simple cli setup

--- cli-php/main.php
+++ cli-php/main.php
@@ -2,4 +2,24 @@
 
 //a simple cli todo app built with php
 
-$todos = array() ;
+$todos = array();
+
+switch ($argv[1]) {
+    case 'all':
+        listAll($todos);
+        break;
+    
+    default:
+        print_r("Invalid option choose a valid option please");
+        break;
+}
+
+function listAll($todos){
+    if (count($todos)==0){
+        print_r('No todo found');
+        return;
+    }
+     foreach($todos as $todo){
+        print_r($todo."\n");
+     }
+}
