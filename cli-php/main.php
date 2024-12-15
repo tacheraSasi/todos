5772230e0e2b5928168c3f223e0c038b086bc624
tacheraSasi <tacherasasi@gmail.com>
2024-12-16 01:01:28 +0300

    _print

--- cli-php/main.php
+++ cli-php/main.php
@@ -10,16 +10,21 @@ switch ($argv[1]) {
         break;
     
     default:
-        print_r("Invalid option choose a valid option please");
+        _print("Invalid option choose a valid option please");
         break;
+}
+
+function _print($msg){
+    print_r($msg."\n");
+    
 }
 
 function listAll($todos){
     if (count($todos)==0){
-        print_r('No todo found');
+        _print('No todo found');
         return;
     }
-     foreach($todos as $todo){
-        print_r($todo."\n");
-     }
+    foreach($todos as $todo){
+        _print($todo."\n");
+    }
 }
